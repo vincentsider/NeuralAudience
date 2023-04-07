ea38f644e8d8742810036caec82e7c0353f96d7e
SanVincento <49484167+vincentsider@users.noreply.github.com>
2023-04-07 22:57:54 +0100

    Comments on line 248 249

--- main.py
+++ main.py
@@ -245,7 +245,7 @@ def home():
 
 
 # Start the Flask app and tell it to listen for incoming HTTP requests
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+#if __name__ == '__main__':
+#    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 
